de602f9c7c51cd15f93495b42688e7114cbc49f8
crtneko97 <simon_kern@hotmail.com>
2023-03-07 22:12:37 +0100

    publishing my homework

---
+++ src/main/java/com/example/demoMedSimon/DemoMedSimonApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoMedSimon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMedSimonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMedSimonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoMedSimon/DemoMedSimonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoMedSimon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMedSimonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
